2c474bed747065d6068018e4103bfec58cfe82b0
4ntonoff <82707546+4ntonoff@users.noreply.github.com>
2021-06-05 14:20:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+x = PrettyTable()
+x.field_names = ["Country", "Capital", "is Russia",]
+x.add_row(["Russia","Moscow","True"])
+x.add_rows([["Argentina", 'Buenos Aires', "False"], ["Jamaica",
+"Kingston", "False"]])
+x.add_column("Starts with A", ["False", "True", "False"])
+print(x)
